ea9b1c2f8d14b2dca13b238e7c5f0983c2881a85
Romsstar <romsstar@gmail.com>
2022-01-10 23:35:44 +0100

    Add project files.

---
+++ DRVEntry.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Text;
+
+namespace WindowsFormsApplication1.Properties
+{
+    public class DRVEntry
+    {
+        public byte bitflag;
+        public byte[] extension;
+        public int offset;
+        public int filesize;
+        public int unk1;
+        public byte[] filename;
+        public byte[] padding;
+        public string extensionString;
+        public string fileNameString;
+        public string fullfileName;
+
+        public DRVEntry(BinaryReader br)
+        {
+            bitflag = br.ReadByte();
+            extension = br.ReadBytes(3);
+            offset = br.ReadInt32() * 0x800;
+            filesize = br.ReadInt32();
+            unk1 = br.ReadInt32();
+            filename = br.ReadBytes(8);
+            padding = br.ReadBytes(8);
+
+            extensionString = Encoding.Default.GetString(extension);
+            fileNameString = Encoding.Default.GetString(filename).Replace("\0", "");
+            fullfileName = fileNameString + "." + extensionString;
+
+        }
+
+    }
+}
---
+++ DRVExtract.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using WindowsFormsApplication1.Properties;
+
+namespace DRVExtract
+{
+    class DRVExtract
+    {
+        public static string dir = "extracted//";
+        public static string input = "";
+        public static List<DRVEntry> GetDRVEntries(BinaryReader br, string dir)
+        {
+            byte[] drvfile;
+            List<DRVEntry> entryList = new List<DRVEntry>();
+
+            while (true)
+            {
+                DRVEntry entry = new DRVEntry(br);
+                bool exists = System.IO.Directory.Exists(dir);
+                System.IO.Directory.CreateDirectory(dir);
+                if (entry.bitflag == 0)
+                    break;
+                entryList.Add(entry);
+                if (entry.filesize != 0)
+                    Console.WriteLine(entry.fullfileName + "| " + entry.offset + "| " + entry.filesize + "| " + entry.unk1 + "|");
+            }
+
+            foreach (var entry in entryList)
+            {
+                br.BaseStream.Seek(entry.offset, SeekOrigin.Begin);
+                drvfile = br.ReadBytes(entry.filesize);
+
+                if (entry.filesize != 0)
+                {
+                    File.WriteAllBytes(dir + entry.fullfileName, drvfile);
+                }
+                if (entry.bitflag == 0x80)
+                {
+                    string subdir = dir + entry.fileNameString + "\\";
+                    GetDRVEntries(br, subdir);
+                }
+            }
+            return entryList;
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Input file:");
+                input = Console.ReadLine();
+            }
+            if (args.Length > 0)
+                input = args[0];
+
+            dir += (Path.GetFileName(input)) + "\\";
+
+
+            using (FileStream fs = new FileStream(input, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                using (BinaryReader br = new BinaryReader(fs, new ASCIIEncoding()))
+                {
+                    GetDRVEntries(br, dir);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
+
